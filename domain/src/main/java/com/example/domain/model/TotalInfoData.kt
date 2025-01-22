bda8eab43d375071afbcce8038c8df8891f08430
saesang <takseha98@gmail.com>
2025-01-22 14:23:41 +0900

    feat(#4): model, repository 구현

---
+++ domain/src/main/java/com/example/domain/model/TotalInfoData.kt
@@ -0,0 +1,9 @@
+package com.example.domain.model
+
+data class TotalInfoData(
+    val date: String,
+    val username: String,
+    val age: Int,
+    val personality: String,
+    val content: String
+)
---
+++ domain/src/main/java/com/example/domain/repository/TotalInfoRepository.kt
@@ -0,0 +1,14 @@
+package com.example.domain.repository
+
+import com.example.domain.model.TotalInfoData
+
+interface TotalInfoRepository {
+    // DB에 데이터 있는지 확인: username이 같고 date가 오늘인 객체가 DB에 존재하면 true 반환
+    suspend fun checkTotalInfoInDb(username: String): Boolean
+
+    // DB 요청
+    suspend fun getTotalInfo(username: String): TotalInfoData?
+
+    // 서버 요청
+    suspend fun fetchTotalInfo(username: String): TotalInfoData
+}
